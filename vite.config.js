c490f9af1099cdba3542138bde60154aed107ade
Julian Nobis <julian.nobis@cgm.com>
2025-07-23 11:17:15 +0200

    adapt prod path

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,29 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import { webcrypto } from 'crypto'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { webcrypto } from "crypto";
 
 // Polyfill the Web Crypto API in Node
-globalThis.crypto = webcrypto
+globalThis.crypto = webcrypto;
 
-export default defineConfig({
-  // Use root "/" in development, and your GitHub Pages sub-path in production
-  /*base: process.env.NODE_ENV === 'production'
+export default defineConfig(({mode}) => {
+  const isProd = mode === "production"
+  return {
+    
+    // Use root "/" in development, and your GitHub Pages sub-path in production
+    /*base: process.env.NODE_ENV === 'production'
     ? '/danieldanlab.github.io/'
     : '/',*/
-    base: './',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    }
-  },
-  plugins: [
-    vue(),
-  ],
-  build: {
-    outDir: 'docs',
-  }
-})
+    base: isProd ? "/" : "./",
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    plugins: [vue()],
+    build: {
+      outDir: "docs",
+    },
+  };
+});
