7fab52a12b2cf33606856d3130d4c5832bf8c8bd
Julian Nobis <julian.nobis@cgm.com>
2025-07-23 17:15:37 +0200

    change base path

--- vite.config.js
+++ vite.config.js
@@ -11,10 +11,10 @@ export default defineConfig(({mode}) => {
   return {
     
     // Use root "/" in development, and your GitHub Pages sub-path in production
-    base: process.env.NODE_ENV === 'production'
+    /*base: process.env.NODE_ENV === 'production'
     ? '/danieldanlab.github.io/'
-    : '/',
-    //base: isProd ? "/" : "./",
+    : '/',*/
+    base: "/",
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
